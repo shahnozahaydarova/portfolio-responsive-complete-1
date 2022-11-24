7169dca4306f77e97478e3c863b7368394133ff0
shahnozahaydarova <shahnozahaydarova03@gmail.com>
2022-11-24 23:08:36 +0500

    portfoleo

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,10 +30,10 @@ function scrollActive(){
         const sectionTop = current.offsetTop - 50
         sectionId = current.getAttribute('id')
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*='+sectionId + ']').classList.add('active')
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
         }
         else{
-            document.querySelector('.nav__menu a[href*='+sectionId + ']').classList.remove('active')
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
         }
 
     })
